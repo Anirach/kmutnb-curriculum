4e357895f2a3d708dfd08b391e7ed11096431fb7
Anirach Mingkhwan <anirach.m@fitm.kmutnb.ac.th>
2025-05-30 10:42:08 +0700

    add ecosystemconfig

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-    apps: [{
-        name: "pm-web",
-        script: "npm",
-        args: ["run", "dev"],
-        env: {
-            HOST: '0.0.0.0',
-            PORT: 3000,
-            DANGEROUSLY_DISABLE_HOST_CHECK: 'true',
-        }
-    }]
-}
+  apps: [
+    {
+      name: "pm-web",
+      script: "npm",
+      args: ["start"],
+      env: {
+        HOST: '0.0.0.0',
+        PORT: 3000,
+        DANGEROUSLY_DISABLE_HOST_CHECK: 'true',
+      }
+    }
+  ]
+};
